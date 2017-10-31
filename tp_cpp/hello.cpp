38f1b3e188ad0291734ebcb5251bf53c9d0ce1cc
Marlon <martianworm17@gmail.com>
2017-11-01 00:46:06 +0800

    C++ Hello World. Initial Commit.

---
+++ tp_cpp/hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 1 Nov 2017
+ *      Author: Lon
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello World";
+   return 0;
+}
+
+
